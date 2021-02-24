5e1d988916b221de148a15244279192253d5c1f0
muneera <gadwalmuneera29@gmail.com>
2021-02-24 21:43:43 +0530

    hello program

---
+++ hello.java
@@ -0,0 +1,17 @@
+class Hello1 {
+    public static void fibonacci(){
+        
+    }
+}
+class Hello2{
+    
+    public static calFactx){}
+    public static void main(String args[]){
+        System.out.println("Hello, World!");
+    }
+     void Fact(){
+        //this new chanegs
+     }
+
+
+}
